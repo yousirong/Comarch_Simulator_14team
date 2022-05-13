798fe51954451e4618a28e6833ddf24d7c06b9ae
EunchanJeong <jec65147@gmail.com>
2022-05-13 10:34:02 +0900

    220513 getOp() & getInstName() 구현

--- Instruction_fetch_decode.c
+++ Instruction_fetch_decode.c
@@ -170,3 +170,226 @@ void decode(InstInfo *instruction)
 	// 		break;
 	// }
 }
+
+char* rTypeName(int fct) {
+	switch (fct) {
+		case 0:
+			return "sll";
+			break;
+		case 2:
+			return "srl";
+			break;
+		case 3:
+			return "sra";
+			break;
+		case 4:
+			return "sllv";
+			break;
+		case 6:
+			return "srlv";
+			break;
+		case 7:
+			return "srav";
+			break;
+		case 8:
+			return "jr";
+			break;
+		case 9:
+			return "jalr";
+			break;
+		case 12:
+			return "syscall";
+			break;
+		case 16:
+			return "mfhi";
+			break;
+		case 17:
+			return "mthi";
+			break;
+		case 18:
+			return "mflo";
+			break;
+		case 19:
+			return "mtlo";
+			break;
+		case 24:
+			return "mult";
+			break;
+		case 25:
+			return "multu";
+			break;
+		case 26:
+			return "div";
+			break;
+		case 27:
+			return "divu";
+			break;
+		case 32:
+			return "add";
+			break;
+		case 33:
+			return "addu";
+			break;
+		case 34:
+			return "sub";
+			break;
+		case 35:
+			return "subu";
+			break;
+		case 36:
+			return "and";
+			break;
+		case 37:
+			return "or";
+			break;
+		case 38:
+			return "xor";
+			break;
+		case 39:
+			return "nor";
+			break;
+		case 42:
+			return "slt";
+			break;
+		case 43:
+			return "sltu";
+			break;
+		default:
+			return "ERROR";
+			break;
+	}
+}
+
+char* iTypeName(int opc) {
+	switch (opc) {
+		case 1:
+			return "bltz";
+			break;
+		case 4:
+			return "beq";
+			break;
+		case 5:
+			return "bne";
+			break;
+		case 6:
+			return "blez";
+			break;
+		case 7:
+			return "bgtz";
+			break;
+		case 8:
+			return "addi";
+			break;
+		case 9:
+			return "addiu";
+			break;
+		case 10:
+			return "slti";
+			break;
+		case 11:
+			return "sltiu";
+			break;
+		case 12:
+			return "andi";
+			break;
+		case 13:
+			return "ori";
+			break;
+		case 14:
+			return "xori";
+			break;
+		case 15:
+			return "lui";
+			break;
+		case 32:
+			return "lb";
+			break;
+		case 33:
+			return "lh";
+			break;
+		case 34:
+			return "lw";
+			break;
+		case 36:
+			return "lbu";
+			break;
+		case 37:
+			return "lhu";
+			break;
+		case 40:
+			return "sb";
+			break;
+		case 41:
+			return "sh";
+			break;
+		case 43:
+			return "sw";
+			break;
+		default:
+			return "ERROR";
+			break;
+	}
+}
+
+char* getInstName(int opc, int fct) {
+
+	switch (opc) {
+		case 0:   	// R-Type 명령어
+			return rTypeName(fct);
+			break;
+		case 2:   	// J-Type 명령어
+			return "j";
+			break;
+		case 3:		// J-Type 명령어	
+			return "jal";
+			break;
+		default:	// I-Type 명령어
+			return iTypeName(opc);
+			break;
+	}
+}
+
+char* getOp(int opc) {
+	// int val = instruction->inst;
+	// int opc = val >> 26;
+
+	switch (opc) {
+	case 0:
+		return "R";
+		break;
+	case 2:
+		return "J";
+		break;
+	case 3:
+		return "J";
+		break;
+	
+	case 1:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13:
+	case 14:
+	case 15:
+	case 32:
+	case 33:
+	case 34:
+	case 36:
+	case 37:
+	case 40:
+	case 41:
+	case 43:
+		return "I";
+		break;
+	default:
+		return "ERROR";
+		break;
+	}
+}
+
