8d7f4f6fa465181f03307a7057076837305b1759
Jaeyun Jung <rangyun36@gmail.com>
2022-05-17 17:15:40 +0900

    220517_파일정리

--- simulator.c
+++ simulator.c
@@ -593,9 +593,9 @@ void instExecute(int opc, int fct, int* isImmediate) {
             case 12: //andi
 				//X는 레지스터 값, Y는 상수
 				unsigned int X;
-				memcpy(X, &isImmediate[0], 4);
+				memcpy(X, &isImmediate[0], 5);
 				unsigned int Y;
-				memcpy(Y, &isImmediate[4], 4);
+				memcpy(Y, &isImmediate[5], 5);
 
 				unsigned int RX = R[X];	//레지스터의 X위치에서 저장된 값 가져오기
 				unsigned int answer = ALU(RX, Y, 8, 0); //c32 == 2, c10 == 0만족하기 위해 C = 8, 제로플래그 0 으로
